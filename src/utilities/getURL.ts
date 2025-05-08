fda831708c50b9637709220ccdcbc3b95bf759c2
sanjaynathani <sanjayrnathani@gmail.com>
2025-05-07 23:20:36 -0700

    vercel url fix

--- src/utilities/getURL.ts
+++ src/utilities/getURL.ts
@@ -1,11 +1,9 @@
 import canUseDOM from './canUseDOM'
 
 export const getServerSideURL = () => {
-  return process.env.VERCEL_TARGET_ENV === "production"
-      ? `https://www.my-custom-domain.com`
-      : process.env.VERCEL_URL
-          ? `https://${process.env.VERCEL_URL}`
-          : `http://localhost:3000`;
+  return process.env.VERCEL_TARGET_ENV === "production" && process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : `http://localhost:3000`;
 }
 
 export const getClientSideURL = () => {
